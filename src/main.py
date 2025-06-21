01d8de29e9cba809263f30fc0c549f99a010670c
djoek <djoek@users.noreply.github.com>
2025-06-21 21:50:08 +0200

    error handling

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+import os
 import logging
 
 logger = logging.getLogger(__name__)
@@ -8,6 +9,12 @@ from dotenv import load_dotenv
 load_dotenv('./.env')
 
 if __name__ == "__main__":
+    try:
+        os.environ['MISTRAL_API_KEY']
+    except KeyError:
+        logger.error('MISTRAL_API_KEY not set. I really need this. ')
+        exit(1)
+
     swotbot.cli_app(
         message="Introduce yourself by name and purpose, and request the user to provide a project description.",
         markdown=True, user="\nUser", stream=True, emoji="", )
