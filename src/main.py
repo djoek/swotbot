fd5bce304b57540bf535d34619a85c7b5842d4b9
djoek <djoek@users.noreply.github.com>
2025-06-21 21:41:11 +0200

    Initial Commit

---
+++ src/main.py
@@ -0,0 +1,14 @@
+import logging
+
+logger = logging.getLogger(__name__)
+
+from swotbot.team import swotbot
+from dotenv import load_dotenv
+
+load_dotenv('./.env')
+
+if __name__ == "__main__":
+    swotbot.cli_app(
+        message="Introduce yourself by name and purpose, and request the user to provide a project description.",
+        markdown=True, user="\nUser", stream=True, emoji="",)
+
